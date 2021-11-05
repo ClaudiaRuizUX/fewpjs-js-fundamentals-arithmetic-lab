7e1cee6288f1acdb228775a7b4cde31854e88346
Claudia <claudiaruizux@gmail.com>
2021-11-06 10:56:10 +1100

    Completed Arithmetic Fundamentals

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+var multiply = function(num1, num2){
+	return num1*num2
+}
 
-// Write your code here
+function random (){
+	let total = Number.parseInt((Math.random() * 10 +1) , 10);
+    //total = Number.parseInt(total, 10)
+	return(total);
+}
+
+
+let num3 = Number();
+let num4 = Number();
+
+function mod (){
+    num3 = 14;
+    num4 = 5;
+    return (num3 % num4);
+}
+
+function max(){
+    let total = Math.max(1, 20);
+    return (total);
+}
